b4bd9e84a8c822189cd7af9fd5a60c36bfb425b7
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2024-01-13 20:41:49 +0200

    - The base version to test with pub dev

--- lib/great_form.dart
+++ lib/great_form.dart
@@ -1,7 +1,23 @@
 library great_form;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class GreatForm extends StatefulWidget {
+  const GreatForm({super.key, required this.fieldCount});
+  final int fieldCount;
+
+  @override
+  State<GreatForm> createState() => _GreatFormState();
+}
+
+class _GreatFormState extends State<GreatForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: [for (int i = 0; i < widget.fieldCount; i++) TextFormField()],
+        ));
+  }
 }
